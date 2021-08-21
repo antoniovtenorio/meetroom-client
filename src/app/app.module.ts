6a4d532bd3c42f90f2093d1c992853ceaf07139a
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-08-21 13:37:40 -0300

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent
   ],
   imports: [
     BrowserModule,
