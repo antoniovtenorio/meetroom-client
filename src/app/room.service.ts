9a8d534d0db7b65f9f89a6511cd725690c9fb6e2
Antonio Tenorio <antonio.v.tenorio@outlook.com>
2021-08-21 14:17:16 -0300

    created room-list component and room interface.

--- src/app/room.service.ts
+++ src/app/room.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RoomService {
+  deleteRoom(id: number) {
+    throw new Error('Method not implemented.');
+  }
+  getRoomList(): import("rxjs").Observable<import("./room").Room[]> {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
